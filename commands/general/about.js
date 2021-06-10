5205900a630a49c01daac4eb92c9d1eacf6fc532
InventBoss <85673323+InventBoss@users.noreply.github.com>
2021-06-10 15:33:03 +0000

    Modified the >about command to include links to the GitHub and to the Top.gg page

--- commands/general/about.js
+++ commands/general/about.js
@@ -9,6 +9,7 @@ module.exports = {
       .setTitle("**About MeDaBot :scroll:**")
       .addField("Description", "This is a discord bot created by InventBoss to advance his coding skills.")
       .addField("Creator", "InventBoss, the creator of MeDaBot, is a loser who co-owns [Nexus](https://discord.gg/YCK2TZbT69).\n\nYou can support him by giving him nitro via. [Nexus](https://discord.gg/YCK2TZbT69) or by donating to him on Hypixel Skyblock while he is online.")
+      .addField("Links", "Github Repo - https://github.com/InventBoss/MeDaBot-JavaScript\nTop.gg Page (Coming soon) - https://top.gg/bot/763313827944202250")
     message.channel.send(embed)
   }
 }
