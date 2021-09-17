4ecfde66ee5fea4f09a644a1043cd09ede77e7b6
InventBoss <marlowlichty@gmail.com>
2021-09-16 21:55:50 -0600

    Test mode, other things

--- commands/developper/test.js
+++ commands/developper/test.js
@@ -1,11 +1,9 @@
-const Discord = require("discord.js")
-
 module.exports = {
 	name: "test",
 	execute(message) {
     if (!message.author.id === "617816411750006794") return
 
-    
+		extra.callError(message, "Test")
 
 	}
 }
