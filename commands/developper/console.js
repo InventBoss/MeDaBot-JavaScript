7831cae95a9165ab5eafd070f8330b14e4dc760d
InventBoss <marlowlichty@gmail.com>
2021-10-20 22:23:26 -0600

    Added a subcommand to check perms

--- commands/developper/console.js
+++ commands/developper/console.js
@@ -1,5 +1,45 @@
 const Discord = require("discord.js")
 
+const permissionList = new Array(
+  "CREATE_INSTANT_INVITE",
+  "KICK_MEMBERS",
+  "BAN_MEMBERS",
+  "ADMINISTRATOR",
+  "MANAGE_CHANNELS",
+  "MANAGE_GUILD",
+  "ADD_REACTIONS",
+  "VIEW_AUDIT_LOG",
+  "PRIORITY_SPEAKER",
+  "STREAM",
+  "VIEW_CHANNEL",
+  "SEND_MESSAGES",
+  "SEND_TTS_MESSAGES",
+  "MANAGE_MESSAGES",
+  "EMBED_LINKS",
+  "ATTACH_FILES",
+  "READ_MESSAGE_HISTORY",
+  "MENTION_EVERYONE",
+  "USE_EXTERNAL_EMOJIS",
+  "VIEW_GUILD_INSIGHTS",
+  "CONNECT",
+  "SPEAK",
+  "MUTE_MEMBERS",
+  "DEAFEN_MEMBERS",
+  "MOVE_MEMBERS",
+  "USE_VAD",
+  "CHANGE_NICKNAME",
+  "MANAGE_NICKNAMES",
+  "MANAGE_ROLES",
+  "MANAGE_WEBHOOKS",
+  "MANAGE_EMOJIS_AND_STICKERS",
+  "USE_APPLICATION_COMMANDS",
+  "REQUEST_TO_SPEAK",
+  "MANAGE_THREADS",
+  "USE_PUBLIC_THREADS",
+  "USE_PRIVATE_THREADS",
+  "USE_EXTERNAL_STICKERS"
+)
+
 module.exports = {
   name: "console",
   execute(message, args) {
@@ -15,7 +55,22 @@ module.exports = {
         message.channel.send(finalMessage)
         message.channel.stopTyping()
       })
+    
+    } else if (args[0] === "permissions") {
+      let obtainedPermissions = ""
 
+      for (let i = 0; i < permissionList.length; i++) {
+        if (message.guild.me.permissions.has([permissionList[i]])) {
+          obtainedPermissions += permissionList[i] + "\n"
+        }
+      }
+
+      const embed = new Discord.MessageEmbed()
+        .setTitle("Bot Permissions")
+        .setDescription(obtainedPermissions)
+        .setColor("#ff8000")
+      message.channel.send({embeds : [embed]})
+    
     } else if (args[0] === "remindaboutrewind") {
       var user = message.guild.members.cache.get(args[1])
       user.send("Hey there! Just a friendly reminder to dislike youtube rewind.\n\nI got the link right here for you:\n\nhttps://www.youtube.com/watch?v=YbJOTdZBX1g")
